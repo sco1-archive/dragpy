62d4f8ed58af0057c08b5ee57c0653af1aef5613
sco1 <sco1.git@gmail.com>
2016-09-06 20:00:54 -0400

    Refactor for generic draggable objects
    Pull core functionality into an inheritable class and move line object to its own class. With each annotation object defining its own initialization and move bahavior, capability can be added more quickly with less copy pasta

--- dragpy.py
+++ dragpy.py
@@ -1,21 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-class DraggableLine:
-    def __init__(self, orientation, ax, position):
-        if orientation.lower() == 'horizontal':
-            self.myline, = ax.plot(ax.get_xlim(), np.array([1, 1])*position)
-            self.orientation = orientation.lower()
-        elif orientation.lower() == 'vertical':
-            self.myline, = ax.plot(np.array([1, 1])*position, ax.get_ylim())
-            self.orientation = orientation.lower()
-        else:
-            # throw an error
-            pass
-
+class DragObj:
+    def __init__(self, ax):
         self.parentcanvas = ax.figure.canvas
         self.parentax = ax
-        self.myline._label = 'dragobj'
 
         self.clickpress = self.parentcanvas.mpl_connect('button_press_event', self.on_click)  # Execute on mouse click
         self.clicked = False
@@ -31,10 +20,10 @@ class DraggableLine:
         self.mousemotion = self.parentcanvas.mpl_connect('motion_notify_event', self.on_motion)
         self.clickrelease = self.parentcanvas.mpl_connect('button_release_event', self.on_release)
         self.clicked = True
-
+    
     def shouldthismove(self, event):
         # Check to see if this object has been clicked on
-        contains, attrs = self.myline.contains(event)
+        contains, attrs = self.myobj.contains(event)
         if not contains:
             # We haven't been clicked
             timetomove = False
@@ -48,26 +37,12 @@ class DraggableLine:
                         firingobjs.append(child)
 
             # Assume the last child object is the topmost rendered object, only move if we're it
-            if firingobjs[-1] == self.myline:
+            if firingobjs[-1] == self.myobj:
                 timetomove = True
             else:
                 timetomove = False
 
         return timetomove
-
-    def on_motion(self, event):
-        # Executed on mouse motion
-        if not self.clicked: return  # See if we've clicked yet
-        if event.inaxes != self.parentax: return # See if we're moving over the parent axes object
-
-        if self.orientation == 'vertical':
-            self.myline.set_xdata(np.array([1, 1])*event.xdata)
-            self.myline.set_ydata(self.parentax.get_ylim())
-        elif self.orientation == 'horizontal':
-            self.myline.set_xdata(self.parentax.get_xlim())
-            self.myline.set_ydata(np.array([1, 1])*event.ydata)
-
-        self.parentcanvas.draw()
 
     def on_release(self, event):
         self.clicked = False
@@ -75,3 +50,42 @@ class DraggableLine:
         self.parentcanvas.mpl_disconnect(self.mousemotion)
         self.parentcanvas.mpl_disconnect(self.clickrelease)
         self.parentcanvas.draw()
+
+class DragLine(DragObj):
+    def __init__(self, orientation, ax, position):
+        DragObj.__init__(self, ax)
+
+        if orientation.lower() == 'horizontal':
+            self.myobj, = ax.plot(ax.get_xlim(), np.array([1, 1])*position)
+            self.orientation = orientation.lower()
+        elif orientation.lower() == 'vertical':
+            self.myobj, = ax.plot(np.array([1, 1])*position, ax.get_ylim())
+            self.orientation = orientation.lower()
+        else:
+            # throw an error
+            pass
+
+        self.myobj._label = 'dragobj'
+
+    def on_motion(self, event):
+        # Executed on mouse motion
+        if not self.clicked: return  # See if we've clicked yet
+        if event.inaxes != self.parentax: return # See if we're moving over the parent axes object
+
+        if self.orientation == 'vertical':
+            self.myobj.set_xdata(np.array([1, 1])*event.xdata)
+            self.myobj.set_ydata(self.parentax.get_ylim())
+        elif self.orientation == 'horizontal':
+            self.myobj.set_xdata(self.parentax.get_xlim())
+            self.myobj.set_ydata(np.array([1, 1])*event.ydata)
+
+        self.parentcanvas.draw()
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+vl1 = DragLine('vertical', ax, 3)
+vl2 = DragLine('vertical', ax, 6)
+
+ax.set_xlim([0, 10])
+plt.show()
