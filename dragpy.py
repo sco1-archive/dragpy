0b5ac10de135346d7ab6d564dc46ef8d32cc05a0
sco1 <sco1.git@gmail.com>
2017-01-13 11:20:26 -0500

    Add lineseries snapping to 2D Dragline
    2D draglines now have the ability to snap to a user-defined lineseries
    
    Changed explicit superclass calls to the implicit super() calls

--- dragpy.py
+++ dragpy.py
@@ -67,7 +67,7 @@ class _DragObj:
 
 class _DragLine(_DragObj):
     def __init__(self, ax):
-        _DragObj.__init__(self, ax)
+        super().__init__(ax)
 
     def on_motion(self, event):
         # Executed on mouse motion
@@ -75,25 +75,53 @@ class _DragLine(_DragObj):
         if event.inaxes != self.parentax: return # See if we're moving over the parent axes object
 
         if self.orientation == 'vertical':
-            self.myobj.set_xdata(np.array([1, 1])*event.xdata)
+            if self.snapto:
+                snaptoxdata = self.snapto.get_xdata()
+                minx = min(snaptoxdata)
+                maxx = max(snaptoxdata)
+                if event.xdata > maxx:
+                    xcoord = maxx
+                elif event.xdata < minx:
+                    xcoord = minx
+                else:
+                    xcoord = event.xdata
+            else:
+                xcoord = event.xdata
+            self.myobj.set_xdata(np.array([1, 1])*xcoord)
             self.myobj.set_ydata(self.parentax.get_ylim())
         elif self.orientation == 'horizontal':
+            if self.snapto:
+                snaptoydata = self.snapto.get_ydata()
+                miny = min(snaptoydata)
+                maxy = max(snaptoydata)
+                if event.ydata > maxy:
+                    ycoord = maxy
+                elif event.ydata < miny:
+                    ycoord = miny
+                else:
+                    ycoord = event.ydata
+            else:
+                ycoord = event.ydata
             self.myobj.set_xdata(self.parentax.get_xlim())
-            self.myobj.set_ydata(np.array([1, 1])*event.ydata)
+            self.myobj.set_ydata(np.array([1, 1])*ycoord)
 
         self.parentcanvas.draw()
 
 
 class _DragPatch(_DragObj):
     def __init__(self, ax, xy):
-        _DragObj.__init__(self, ax)
+        super().__init__(ax)
     
         self.oldxy = xy  # Store for motion callback
 
     def on_motion(self, event):
         # Executed on mouse motion
-        if not self.clicked: return  # See if we've clicked yet
-        if event.inaxes != self.parentax: return # See if we're moving over the parent axes object
+        if not self.clicked:
+             # See if we've clicked yet
+            return
+        if event.inaxes != self.parentax: 
+             # See if we're moving over the parent axes object
+            return
 
         oldx, oldy = self.oldxy
         dx = event.xdata - self.clickx
@@ -125,7 +153,7 @@ class _DragPatch(_DragObj):
     
 
 class DragLine2D(_DragLine):
-    def __init__(self, ax, position, orientation='vertical', **kwargs):
+    def __init__(self, ax, position, orientation='vertical', snapto=None, **kwargs):
         if orientation.lower() == 'horizontal':
             self.myobj = lines.Line2D(ax.get_xlim(), np.array([1, 1])*position, **kwargs)
             self.orientation = orientation.lower()
@@ -133,11 +161,13 @@ class DragLine2D(_DragLine):
             self.myobj = lines.Line2D(np.array([1, 1])*position, ax.get_ylim(), **kwargs)
             self.orientation = orientation.lower()
         else:
-            # throw an error
-            pass
-        
+            raise ValueError(f"Unsupported orientation string: '{orientation}'")
+
+        # TODO: Check to make sure snapto is a valid lineseries (or None)
+        self.snapto = snapto
+
         ax.add_artist(self.myobj)
-        _DragLine.__init__(self, ax)
+        super().__init__(ax)
 
 
 class DragEllipse(_DragPatch):
@@ -145,7 +175,7 @@ class DragEllipse(_DragPatch):
         self.myobj = patches.Ellipse(xy, width, height, angle, **kwargs)
         ax.add_artist(self.myobj)
 
-        _DragPatch.__init__(self, ax, xy)
+        super().__init__(ax, xy)
 
 
 class DragCircle(DragEllipse):
@@ -153,7 +183,7 @@ class DragCircle(DragEllipse):
         self.myobj = patches.Circle(xy, radius, **kwargs)
         ax.add_artist(self.myobj)
 
-        _DragPatch.__init__(self, ax, xy)
+        super().__init__(ax, xy)
 
 
 class DragRectangle(_DragPatch):
@@ -161,7 +191,7 @@ class DragRectangle(_DragPatch):
         self.myobj = patches.Rectangle(xy, width, height, angle, **kwargs)
         ax.add_artist(self.myobj)
 
-        _DragPatch.__init__(self, ax, xy)
+        super().__init__(ax, xy)
 
 
 class DragArc(_DragPatch):
@@ -169,7 +199,7 @@ class DragArc(_DragPatch):
         self.myobj = patches.Arc(xy, width, height, angle, theta1, theta2, **kwargs)
         ax.add_artist(self.myobj)
 
-        _DragPatch.__init__(self, ax, xy)
+        super().__init__(ax, xy)
 
 
 class DragWedge(_DragPatch):
@@ -177,7 +207,7 @@ class DragWedge(_DragPatch):
         self.myobj = patches.Wedge(center, r, theta1, theta2, width, **kwargs)
         ax.add_artist(self.myobj)
 
-        _DragPatch.__init__(self, ax, center)
+        super().__init__(ax, center)
 
 
 class DragRegularPolygon(_DragPatch):
@@ -185,4 +215,4 @@ class DragRegularPolygon(_DragPatch):
         self.myobj = patches.RegularPolygon(xy, numVertices, radius, orientation, **kwargs)
         ax.add_artist(self.myobj)
 
-        _DragPatch.__init__(self, ax, xy)
+        super().__init__(ax, xy)
